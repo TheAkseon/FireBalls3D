4180d7b39996c7480c9d93af65f8af05abc5e6d4
Kirill <abqwerty390@gmail.com>
2023-10-31 16:07:42 +0300

    added bullet flight

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,17 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
 }
